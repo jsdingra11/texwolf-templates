dbae0691a3b09ec1f8436137789ad447aafaea67
Jashanpreet Singh Dingra <125212173+jsdingra11@users.noreply.github.com>
2026-02-13 12:41:13 +0530

    Update config.js

--- connect-sci/configrations/config.js
+++ connect-sci/configrations/config.js
@@ -1,24 +1,17 @@
 // --- CONFIGURATION ---
-
-// GitHub Token split into 3 parts
 const GH_TOKEN_P1 = "ghp_KufdJL";
 const GH_TOKEN_P2 = "eDstzUItmpT30";
 const GH_TOKEN_P3 = "kz2L2KNIoS80ftQlh";
-
 const GH_TOKEN = GH_TOKEN_P1 + GH_TOKEN_P2 + GH_TOKEN_P3;
-
 const GH_OWNER = "jsdingra11";
 const GH_REPO = "texwolf-templates";
 const GH_BRANCH = "main";
 
 
 // --- FIREBASE CONFIG ---
-
-// Firebase API Key split into 3 parts
 const FB_APIKEY_P1 = "AIzaSyB4e";
 const FB_APIKEY_P2 = "oofAdY4DrW";
 const FB_APIKEY_P3 = "-VKvfGJl5aNv2hoCTn4c";
-
 const firebaseConfig = {
   apiKey: FB_APIKEY_P1 + FB_APIKEY_P2 + FB_APIKEY_P3,
   authDomain: "iist-fd6e8.firebaseapp.com",
@@ -29,10 +22,7 @@ const firebaseConfig = {
   appId: "1:692699808449:web:120c8941a940d071f24a40",
   measurementId: "G-GPFG8LWF2H"
 };
-
-
-// --- INITIALIZE FIREBASE ---
-
-if (!firebase.apps.length) {
+if (typeof firebase !== 'undefined' && !firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
+const FIREWORKS_API_KEY = "fw_5bXHj2Au4ZHxodhMdAG3Lf";
